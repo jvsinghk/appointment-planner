d49581bd16fd032dbcaa4ee1f5bc5a8cda056ee7
Jasvinder Singh <jvsinghk@gmail.com>
2021-05-30 01:19:33 +0530

    Complete ContactForm requirements

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,39 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <label for='name'>Name:</label>
+      <input 
+        id='name' 
+        className='name'
+        type='text' 
+        value={name} 
+        onChange={({target}) => (setName(target.value))}
+        required
+      />
+
+      <label for='phone'>Phone:</label>
+      <input 
+        id='number' 
+        className='phone'
+        type='tel' 
+        value={phone} 
+        pattern='^[6-9][0-9]{9}$'
+        onChange={({target}) => (setPhone(target.value))}
+        required
+      />
+
+      <label for='email'>Email:</label>
+      <input 
+        id='email'
+        className='email' 
+        type='email' 
+        value={email}
+        onChange={({target}) => (setEmail(target.value))} 
+        required
+      />
+
+      <button type='submit'>Submit</button>
+    </form>
   );
 };
