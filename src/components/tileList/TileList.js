7f8acd940e51c0f509bce5fa8fe063ea7051ae82
Jasvinder Singh <jvsinghk@gmail.com>
2021-05-30 01:26:41 +0530

    Complete TileList requirements

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = (props) => {
+  const { tiles } = props;
   return (
     <div>
-      
+      {tiles.map((value, index) => 
+      <Tile value={value}  key={index} />)}
     </div>
   );
 };
